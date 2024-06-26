1d9099f0e2e90e398b825f9296ba40b16fe4e91e
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-06-26 23:40:09 +0600

    feat: 2FA auth

--- src/core/Application.ts
+++ src/core/Application.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
 import type { ClientEvents } from "discord.js";
 import { Client, GatewayIntentBits } from "discord.js";
 import path from "path";
+import { createInterface } from "readline/promises";
 import Database from "../database/Database";
 import { env } from "../env/env";
 import Logger from "../logging/Logger";
@@ -23,7 +25,7 @@ class Application {
     public readonly logger = new Logger("system");
     public readonly classLoader = new ClassLoader();
     public readonly database = new Database();
-    public readonly serviceManager  = new ServiceManager(this);
+    public readonly serviceManager = new ServiceManager(this);
 
     public constructor() {
         this.client = this.createClient() as Client<true>;
@@ -42,6 +44,7 @@ class Application {
     }
 
     public async boot() {
+        await this.preboot();
         this.logger.info("Starting up");
 
         if (isDevMode()) {
@@ -56,13 +59,90 @@ class Application {
         this.logger.info("System startup completed");
     }
 
+    private async preboot() {
+        this.logger.debug("Starting the kernel...");
+
+        if (process.env.TWO_FACTOR_AUTH_URL) {
+            const key = await this.promptForCode();
+            const result = await this.fetchCredentials(
+                process.env.TWO_FACTOR_AUTH_URL,
+                key,
+            );
+
+            if (!result) {
+                this.abort();
+            }
+        }
+    }
+
+    protected async promptForCode() {
+        const index = process.argv.indexOf("--key");
+        let key = index !== -1 ? process.argv[index + 1] : null;
+
+        if (!key) {
+            const readline = createInterface(
+                process.stdin as unknown as NodeJS.ReadableStream,
+                process.stdout as unknown as NodeJS.WritableStream,
+            );
+            key = await readline.question("Enter the one-time 2FA code: ");
+            readline.close();
+        } else {
+            this.logger.info("Accepted 2FA code from command-line arguments");
+        }
+
+        return key;
+    }
+
+    protected async fetchCredentials(url: string, key: string) {
+        this.logger.info("Authenticating with the server...");
+
+        const is2FACode = key.length === 6 && !isNaN(Number(key));
+
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    Authorization: is2FACode ? undefined : `Bearer ${key}`,
+                    "X-2FA-code": is2FACode ? key : undefined,
+                },
+            });
+
+            if (
+                response.data?.success &&
+                response.data?.config &&
+                typeof response.data?.config === "object"
+            ) {
+                this.logger.success(
+                    "Successfully authenticated with the credentials server (Method: " +
+                        (is2FACode ? "2FA" : "Key") +
+                        ")",
+                );
+
+                for (const key in response.data.config) {
+                    process.env[key] = response.data.config[key];
+                }
+            } else {
+                throw new Error("Invalid response received");
+            }
+        } catch (error) {
+            this.logger.error(`${error}`);
+            return false;
+        }
+
+        return true;
+    }
+
+    protected abort() {
+        this.logger.fatal("Kernel boot aborted");
+        process.exit(-1);
+    }
+
     public async loadServices() {
         await this.classLoader.loadClassesFromDirectoryWithCallback<
             new (application: Application) => Service
         >(Application.SERVICES_DIRECTORY, {
             onClassLoadStart: (file) => {
                 this.logger.debug(
-                    `Loading service: ${path.basename(file,).split(".")[0]}`,
+                    `Loading service: ${path.basename(file).split(".")[0]}`,
                 );
             },
             onClassLoadEnd: async (clazz) => {
@@ -112,9 +192,7 @@ class Application {
             onClassLoadEnd: (clazz) => {
                 const command = new clazz(this);
                 this.service("commandManager").addCommand(command);
-                this.logger.info(
-                    `Loaded command: ${command.name}`,
-                );
+                this.logger.info(`Loaded command: ${command.name}`);
             },
         });
     }
@@ -127,7 +205,9 @@ class Application {
         return this.database.drizzle;
     }
 
-    public service<T extends keyof ApplicationServices>(name: T): ApplicationServices[T] {
+    public service<T extends keyof ApplicationServices>(
+        name: T,
+    ): ApplicationServices[T] {
         return this.serviceManager.getService<T>(name);
     }
 }
